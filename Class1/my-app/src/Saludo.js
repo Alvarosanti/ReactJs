3f3c9bf0be96c334bb6116d64ffbb23f597cda63
Alvaro Rodrigo <alvarosantisteban56@gmail.com>
2021-04-10 07:55:33 -0500

    Update

---
+++ Class1/my-app/src/Saludo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const saludo = () =>{
+    return(
+        <div>
+            <h2>
+                Hola, soy Arenita!
+            </h2>
+            <p>
+                Este es un mensaje de funcion componente React!
+            </p>
+        </div>
+    )
+}
+
+export default saludo;
